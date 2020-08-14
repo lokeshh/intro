d9814ff73d8727dd0f80c891f2fb3405ce95c07d
Lokesh Sharma <lokeshhsharma@gmail.com>
2020-08-14 17:51:38 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ export default () => (
     <h1 className={styles.headline}>Lokesh Sharma</h1>
     <p className={styles.underline}>
       I'm a <strong>Back-End</strong> Engineer.<br/>
-      <strong>Linux</strong> and <string>Open Source</strong> enthusiast.
+      <strong>Linux</strong> and <strong>Open Source</strong> enthusiast.
     </p>
   </>
 )
